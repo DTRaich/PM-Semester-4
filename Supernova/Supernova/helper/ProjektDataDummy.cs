d0b954072716505d67539644d8af201cb5a87a19
DTRaich <trefzeda@dhbw-loerrach.de>
2014-05-09 12:58:57 +0200

    projektdatadummy zur erstellung eingeührt

---
+++ Supernova/Supernova/helper/ProjektDataDummy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Supernova.helper
+{
+    public class ProjektDataDummy
+    {
+        #region fields 
+
+        #endregion
+        public ProjektDataDummy()
+        {
+
+        }
+    }
+}
