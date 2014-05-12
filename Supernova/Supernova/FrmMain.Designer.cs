45f620bf1827eb76a751f459d314a278732f53fb
eschn <nils-esch@hotmail.de>
2014-05-12 08:52:01 +0200

    Admin in neues tab

--- Supernova/Supernova/FrmMain.Designer.cs
+++ Supernova/Supernova/FrmMain.Designer.cs
@@ -30,13 +30,9 @@
         {
             this.msMenu = new System.Windows.Forms.MenuStrip();
             this.projekteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.projektErstellenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.projekteAnzeigenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.adminToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.kriterienPriorisierenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.rechteverwaltungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.abteilungskapazitätenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.budgetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projektErstellenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projekteÄndernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.accountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.benutzerinformationenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.passwordÄndernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -46,7 +42,11 @@
             this.überToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sStateStrip = new System.Windows.Forms.StatusStrip();
             this.pnlMain = new System.Windows.Forms.Panel();
-            this.projekteÄndernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kriterienPriorisierenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rechteverwaltungToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abteilungskapazitätToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.budgetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.benutzerAnlegenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.msMenu.SuspendLayout();
             this.SuspendLayout();
@@ -56,6 +56,7 @@
             this.msMenu.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.projekteToolStripMenuItem,
+            this.adminToolStripMenuItem1,
             this.accountToolStripMenuItem,
             this.adminToolStripMenuItem});
             this.msMenu.Location = new System.Drawing.Point(0, 0);
@@ -69,19 +70,11 @@
             this.projekteToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.projekteAnzeigenToolStripMenuItem,
             this.projektErstellenToolStripMenuItem,
-            this.projekteÄndernToolStripMenuItem,
-            this.adminToolStripMenuItem1});
+            this.projekteÄndernToolStripMenuItem});
             this.projekteToolStripMenuItem.Enabled = false;
             this.projekteToolStripMenuItem.Name = "projekteToolStripMenuItem";
             this.projekteToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
             this.projekteToolStripMenuItem.Text = "Projekte";
-            // 
-            // projektErstellenToolStripMenuItem
-            // 
-            this.projektErstellenToolStripMenuItem.Name = "projektErstellenToolStripMenuItem";
-            this.projektErstellenToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
-            this.projektErstellenToolStripMenuItem.Text = "Projekt erstellen";
-            this.projektErstellenToolStripMenuItem.Click += new System.EventHandler(this.projektErstellenToolStripMenuItem_Click);
             // 
             // projekteAnzeigenToolStripMenuItem
             // 
@@ -90,44 +83,18 @@
             this.projekteAnzeigenToolStripMenuItem.Text = "Projekte anzeigen";
             this.projekteAnzeigenToolStripMenuItem.Click += new System.EventHandler(this.projekteAnzeigenToolStripMenuItem_Click);
             // 
-            // adminToolStripMenuItem1
+            // projektErstellenToolStripMenuItem
             // 
-            this.adminToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.kriterienPriorisierenToolStripMenuItem,
-            this.rechteverwaltungToolStripMenuItem,
-            this.abteilungskapazitätenToolStripMenuItem,
-            this.budgetToolStripMenuItem,
-            this.benutzerAnlegenToolStripMenuItem});
-            this.adminToolStripMenuItem1.Name = "adminToolStripMenuItem1";
-            this.adminToolStripMenuItem1.Size = new System.Drawing.Size(175, 22);
-            this.adminToolStripMenuItem1.Text = "Admin";
+            this.projektErstellenToolStripMenuItem.Name = "projektErstellenToolStripMenuItem";
+            this.projektErstellenToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.projektErstellenToolStripMenuItem.Text = "Projekt erstellen";
+            this.projektErstellenToolStripMenuItem.Click += new System.EventHandler(this.projektErstellenToolStripMenuItem_Click);
             // 
-            // kriterienPriorisierenToolStripMenuItem
+            // projekteÄndernToolStripMenuItem
             // 
-            this.kriterienPriorisierenToolStripMenuItem.Name = "kriterienPriorisierenToolStripMenuItem";
-            this.kriterienPriorisierenToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
-            this.kriterienPriorisierenToolStripMenuItem.Text = "Kriterien priorisieren";
-            this.kriterienPriorisierenToolStripMenuItem.Click += new System.EventHandler(this.kriterienPriorisierenToolStripMenuItem_Click);
-            // 
-            // rechteverwaltungToolStripMenuItem
-            // 
-            this.rechteverwaltungToolStripMenuItem.Name = "rechteverwaltungToolStripMenuItem";
-            this.rechteverwaltungToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
-            this.rechteverwaltungToolStripMenuItem.Text = "Rechteverwaltung";
-            this.rechteverwaltungToolStripMenuItem.Click += new System.EventHandler(this.rechteverwaltungToolStripMenuItem_Click);
-            // 
-            // abteilungskapazitätenToolStripMenuItem
-            // 
-            this.abteilungskapazitätenToolStripMenuItem.Name = "abteilungskapazitätenToolStripMenuItem";
-            this.abteilungskapazitätenToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
-            this.abteilungskapazitätenToolStripMenuItem.Text = "Abteilungskapazitäten";
-            this.abteilungskapazitätenToolStripMenuItem.Click += new System.EventHandler(this.abteilungskapazitätenToolStripMenuItem_Click);
-            // 
-            // budgetToolStripMenuItem
-            // 
-            this.budgetToolStripMenuItem.Name = "budgetToolStripMenuItem";
-            this.budgetToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
-            this.budgetToolStripMenuItem.Text = "Budget";
+            this.projekteÄndernToolStripMenuItem.Name = "projekteÄndernToolStripMenuItem";
+            this.projekteÄndernToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.projekteÄndernToolStripMenuItem.Text = "Projekte ändern";
             // 
             // accountToolStripMenuItem
             // 
@@ -173,14 +140,14 @@
             // hilfekatalogToolStripMenuItem
             // 
             this.hilfekatalogToolStripMenuItem.Name = "hilfekatalogToolStripMenuItem";
-            this.hilfekatalogToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.hilfekatalogToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
             this.hilfekatalogToolStripMenuItem.Text = "Hilfekatalog";
             this.hilfekatalogToolStripMenuItem.Click += new System.EventHandler(this.hilfekatalogToolStripMenuItem_Click);
             // 
             // überToolStripMenuItem
             // 
             this.überToolStripMenuItem.Name = "überToolStripMenuItem";
-            this.überToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.überToolStripMenuItem.Size = new System.Drawing.Size(142, 22);
             this.überToolStripMenuItem.Text = "Über";
             this.überToolStripMenuItem.Click += new System.EventHandler(this.überToolStripMenuItem_Click);
             // 
@@ -200,18 +167,47 @@
             this.pnlMain.Size = new System.Drawing.Size(945, 381);
             this.pnlMain.TabIndex = 2;
             // 
-            // projekteÄndernToolStripMenuItem
+            // adminToolStripMenuItem1
             // 
-            this.projekteÄndernToolStripMenuItem.Name = "projekteÄndernToolStripMenuItem";
-            this.projekteÄndernToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
-            this.projekteÄndernToolStripMenuItem.Text = "Projekte ändern";
+            this.adminToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kriterienPriorisierenToolStripMenuItem,
+            this.rechteverwaltungToolStripMenuItem,
+            this.abteilungskapazitätToolStripMenuItem,
+            this.budgetToolStripMenuItem,
+            this.benutzerAnlegenToolStripMenuItem});
+            this.adminToolStripMenuItem1.Name = "adminToolStripMenuItem1";
+            this.adminToolStripMenuItem1.Size = new System.Drawing.Size(55, 20);
+            this.adminToolStripMenuItem1.Text = "Admin";
+            // 
+            // kriterienPriorisierenToolStripMenuItem
+            // 
+            this.kriterienPriorisierenToolStripMenuItem.Name = "kriterienPriorisierenToolStripMenuItem";
+            this.kriterienPriorisierenToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.kriterienPriorisierenToolStripMenuItem.Text = "Kriterien priorisieren";
+            // 
+            // rechteverwaltungToolStripMenuItem
+            // 
+            this.rechteverwaltungToolStripMenuItem.Name = "rechteverwaltungToolStripMenuItem";
+            this.rechteverwaltungToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.rechteverwaltungToolStripMenuItem.Text = "Rechteverwaltung";
+            // 
+            // abteilungskapazitätToolStripMenuItem
+            // 
+            this.abteilungskapazitätToolStripMenuItem.Name = "abteilungskapazitätToolStripMenuItem";
+            this.abteilungskapazitätToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.abteilungskapazitätToolStripMenuItem.Text = "Abteilungskapazität";
+            // 
+            // budgetToolStripMenuItem
+            // 
+            this.budgetToolStripMenuItem.Name = "budgetToolStripMenuItem";
+            this.budgetToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
+            this.budgetToolStripMenuItem.Text = "Budget";
             // 
             // benutzerAnlegenToolStripMenuItem
             // 
             this.benutzerAnlegenToolStripMenuItem.Name = "benutzerAnlegenToolStripMenuItem";
-            this.benutzerAnlegenToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.benutzerAnlegenToolStripMenuItem.Size = new System.Drawing.Size(191, 22);
             this.benutzerAnlegenToolStripMenuItem.Text = "Benutzer anlegen";
-            this.benutzerAnlegenToolStripMenuItem.Click += new System.EventHandler(this.benutzerAnlegenToolStripMenuItem_Click);
             // 
             // FrmMain
             // 
@@ -244,17 +240,17 @@
         private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem projektErstellenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem projekteAnzeigenToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem1;
-        private System.Windows.Forms.ToolStripMenuItem kriterienPriorisierenToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem rechteverwaltungToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem abteilungskapazitätenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem benutzerinformationenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem passwordÄndernToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem hilfekatalogToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem überToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem budgetToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ausloggenToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem projekteÄndernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kriterienPriorisierenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rechteverwaltungToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abteilungskapazitätToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem budgetToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem benutzerAnlegenToolStripMenuItem;
     }
 }
