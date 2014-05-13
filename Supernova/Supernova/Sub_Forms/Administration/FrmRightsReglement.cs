c10a5f093c2de86c069efae20fad55cfa0867c68
SinaS93 <sina.steck@googlemail.com>
2014-05-13 13:16:26 +0200

    rechteverwaltung
    erstellt

--- Supernova/Supernova/Sub_Forms/Administration/FrmRightsReglement.cs
+++ Supernova/Supernova/Sub_Forms/Administration/FrmRightsReglement.cs
@@ -6,14 +6,37 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Supernova.data;
 
 namespace Supernova.Sub_Forms.Administration
 {
     public partial class FrmRightsReglement : Form
     {
+        DataTable userGroups = new DataTable();
+
         public FrmRightsReglement()
         {
             InitializeComponent();
+            initializeGroups();
+        }
+
+        private void initializeGroups()
+        {
+            ParameterLoad pl = new ParameterLoad();
+            userGroups = pl.loadUserGroups();
+
+            try
+            {   
+
+            cbUserGroups.DataSource = userGroups;
+            cbUserGroups.ValueMember = "USER_GROUPS_ID";
+            cbUserGroups.DisplayMember = "UG_NAME";
+               
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
