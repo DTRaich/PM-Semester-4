ca3eed1b0ab80f180173c6a5ae80abd9cc2dc667
SinaS93 <sina.steck@googlemail.com>
2014-05-14 13:18:31 +0200

    neue form
    xds

---
+++ Supernova/Supernova/Sub_Forms/Administration/FrmNewDep.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Supernova.Sub_Forms.Administration
+{
+    partial class FrmNewDep
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmNewDep
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 262);
+            this.Name = "FrmNewDep";
+            this.Text = "FrmNewDep";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
