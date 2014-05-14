da94983d0a85451b480cc0bb4b8933bf3732f6f5
SinaS93 <sina.steck@googlemail.com>
2014-05-14 10:54:44 +0200

    cxyvysd
    yxxy

--- Supernova/Supernova/Sub_Forms/Administration/FrmDepartmentSpecifikation.cs
+++ Supernova/Supernova/Sub_Forms/Administration/FrmDepartmentSpecifikation.cs
@@ -6,14 +6,39 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Supernova.data;
 
 namespace Supernova.Sub_Forms.Administration
 {
     public partial class FrmDepartmentSpecifikation : Form
     {
+        DataTable departments;
+
         public FrmDepartmentSpecifikation()
         {
             InitializeComponent();
+            prepareBoxes();
+        }
+
+        private void prepareBoxes()
+        {
+            ParameterLoad pl = new ParameterLoad();
+            departments = pl.loadDeparments();
+           
+            cbDepartments.DataSource = departments;
+            cbDepartments.ValueMember = "DEPARTMENTS_ID";
+            cbDepartments.DisplayMember = "D_NAME";
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
         }
 
 
