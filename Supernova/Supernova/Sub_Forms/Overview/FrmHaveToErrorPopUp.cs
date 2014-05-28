af7953b1fc47944f922fc2f9df84b543df5afc59
DTRaich <trefzeda@dhbw-loerrach.de>
2014-05-28 15:11:38 +0200

    sfsdf

--- Supernova/Supernova/Sub_Forms/Overview/FrmHaveToErrorPopUp.cs
+++ Supernova/Supernova/Sub_Forms/Overview/FrmHaveToErrorPopUp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Supernova.data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +12,83 @@ namespace Supernova.Sub_Forms.Overview
 {
     public partial class FrmHaveToErrorPopUp : Form
     {
+        private DataTable val;
+
         public FrmHaveToErrorPopUp()
         {
             InitializeComponent();
         }
+
+        public FrmHaveToErrorPopUp(DataTable val)
+        {
+           this.val = val;
+           PrepareGrid();
+           Leader l = Leader.getLeaderInst();        
+        }
+
+        private void PrepareGrid()
+        {
+            val.Columns.Add("Muss-Projekt", System.Type.GetType("System.Boolean"));
+
+            foreach (DataRow dataRow in val.Rows)
+            {
+                if (dataRow["P_HaveTo"].ToString().Equals("1"))
+                {
+                    dataRow["Muss-Projekt"] = true;
+                }
+                else
+                {
+                    dataRow["Muss-Projekt"] = false;
+                }
+            }
+            val.AcceptChanges();
+
+            errorGrid.DataSource = val;
+            errorGrid.Columns["P_HaveTo"].Visible = false;
+
+            foreach (DataGridViewColumn col in errorGrid.Columns)
+            {
+                if (col.HeaderText == "Muss-Projekt")
+                {
+                    col.ReadOnly = false;
+                }
+                else
+                {
+                    col.ReadOnly = true;
+                }
+
+            }
+        }
+
+        private void errorGrid_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            ValidationData vl = new ValidationData();
+
+            try
+            {
+                int currentId = Convert.ToInt32(errorGrid[0,e.RowIndex].Value);
+                DataTable dt = vl.SaveAndValidateHaveTo(currentId,0);
+                if (dt.Rows.Count > 0)
+                {
+                    val = null;
+                    val = new DataTable();
+                    val = dt.Copy();
+                    PrepareGrid();
+                }
+                else
+                {
+                    Leader lead = Leader.getLeaderInst();
+                    lead.LoadProjektView();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+                
+        }
+   
+    
     }
 }
