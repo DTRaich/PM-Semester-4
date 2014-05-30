f67441524429c591cadbde7f3412476a713fb00b
DTRaich <trefzeda@dhbw-loerrach.de>
2014-05-30 16:23:47 +0200

    weise riese singleton

---
+++ Supernova/Supernova/helper/WeiserRieseHelperSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Supernova.helper
+{
+    public enum DBTYPES
+    {
+        Oracle,
+        MYSQL,
+        SQL
+    }
+
+    public class WeiserRieseHelperSingleton
+    {
+        private static WeiserRieseHelperSingleton instance;
+        public static WeiserRieseHelperSingleton getInstance()
+        {
+           if (instance == null)
+            {
+                instance = new WeiserRieseHelperSingleton();
+            }
+            return instance;
+        }
+    }
+
+}
