ba7a5e92677fa7768d5ecd834636159d066e8f84
DTRaich <trefzeda@dhbw-loerrach.de>
2014-05-30 19:48:17 +0200

    loaderheper

---
+++ Supernova/Supernova/data/AnalysisLoad.cs
@@ -0,0 +1,14 @@
+﻿using Supernova.helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Supernova.data
+{
+    class AnalysisLoad
+    {
+        string conSting = "Database=fallstudie;Data Source=188.226.215.238;User Id=user1;Password=password";
+        DBerror dbError = DBerror.getInstanze();
+    }
+}
