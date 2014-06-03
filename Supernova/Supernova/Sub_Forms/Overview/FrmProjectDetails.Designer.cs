3ce363bb71f64913f3569e0efbcf49f36064ede0
eschn <nils-esch@hotmail.de>
2014-06-03 10:06:22 +0200

    Details Excel export

--- Supernova/Supernova/Sub_Forms/Overview/FrmProjectDetails.Designer.cs
+++ Supernova/Supernova/Sub_Forms/Overview/FrmProjectDetails.Designer.cs
@@ -34,6 +34,7 @@
             this.txtName = new System.Windows.Forms.TextBox();
             this.txtPoints = new System.Windows.Forms.TextBox();
             this.lblHeader = new System.Windows.Forms.Label();
+            this.btnExcelExport = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.detailsGrid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,19 +46,18 @@
             this.detailsGrid.AllowUserToResizeRows = false;
             this.detailsGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.detailsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.detailsGrid.Location = new System.Drawing.Point(93, 153);
+            this.detailsGrid.Location = new System.Drawing.Point(67, 151);
             this.detailsGrid.Name = "detailsGrid";
             this.detailsGrid.ReadOnly = true;
             this.detailsGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.detailsGrid.Size = new System.Drawing.Size(468, 150);
+            this.detailsGrid.Size = new System.Drawing.Size(506, 150);
             this.detailsGrid.TabIndex = 0;
-            this.detailsGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.detailsGrid_CellContentClick);
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(173, 74);
+            this.lblName.Location = new System.Drawing.Point(64, 76);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(87, 17);
             this.lblName.TabIndex = 1;
@@ -67,7 +67,7 @@
             // 
             this.lblPoints.AutoSize = true;
             this.lblPoints.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPoints.Location = new System.Drawing.Point(173, 111);
+            this.lblPoints.Location = new System.Drawing.Point(64, 113);
             this.lblPoints.Name = "lblPoints";
             this.lblPoints.Size = new System.Drawing.Size(121, 17);
             this.lblPoints.TabIndex = 2;
@@ -75,7 +75,7 @@
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(328, 73);
+            this.txtName.Location = new System.Drawing.Point(219, 75);
             this.txtName.Name = "txtName";
             this.txtName.ReadOnly = true;
             this.txtName.Size = new System.Drawing.Size(138, 20);
@@ -83,7 +83,7 @@
             // 
             // txtPoints
             // 
-            this.txtPoints.Location = new System.Drawing.Point(328, 110);
+            this.txtPoints.Location = new System.Drawing.Point(219, 112);
             this.txtPoints.Name = "txtPoints";
             this.txtPoints.ReadOnly = true;
             this.txtPoints.Size = new System.Drawing.Size(138, 20);
@@ -99,12 +99,23 @@
             this.lblHeader.TabIndex = 5;
             this.lblHeader.Text = "Projektdetails";
             // 
+            // btnExcelExport
+            // 
+            this.btnExcelExport.Location = new System.Drawing.Point(476, 110);
+            this.btnExcelExport.Name = "btnExcelExport";
+            this.btnExcelExport.Size = new System.Drawing.Size(97, 23);
+            this.btnExcelExport.TabIndex = 6;
+            this.btnExcelExport.Text = "Excel Export";
+            this.btnExcelExport.UseVisualStyleBackColor = true;
+            this.btnExcelExport.Click += new System.EventHandler(this.btnExcelExport_Click);
+            // 
             // FrmProjectDetails
             // 
             this.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(646, 332);
+            this.Controls.Add(this.btnExcelExport);
             this.Controls.Add(this.lblHeader);
             this.Controls.Add(this.txtPoints);
             this.Controls.Add(this.txtName);
@@ -131,5 +142,6 @@
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.TextBox txtPoints;
         private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Button btnExcelExport;
     }
 }
--- Supernova/Supernova/Sub_Forms/Overview/FrmProjectDetails.cs
+++ Supernova/Supernova/Sub_Forms/Overview/FrmProjectDetails.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using Supernova.data;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Supernova.Sub_Forms.Overview
 {
@@ -46,14 +47,84 @@ namespace Supernova.Sub_Forms.Overview
         {
             txtName.Text = currentProjectName;
             txtPoints.Text = currentPoints.ToString();
-            
+
         }
 
-        private void detailsGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private void btnExcelExport_Click(object sender, EventArgs e)
         {
-
+            ExcelExport();
         }
 
+        private void ExcelExport()
+        {
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            Int16 i, j;
+
+            xlApp = new Excel.ApplicationClass();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[1, 1] = lblName.Text;
+            xlWorkSheet.Cells[1, 2] = currentProjectName;
+            xlWorkSheet.Cells[1, 3] = lblPoints.Text;
+            xlWorkSheet.Cells[1, 4] = currentPoints;
+            
+            for (i = 0; i < detailsGrid.ColumnCount ; i++)
+            {
+                xlWorkSheet.Cells[2, i+1] = detailsGrid.Columns[i].Name.ToString();
+            }
+
+            for (i = 0; i <= detailsGrid.RowCount -1; i++)
+            {
+                for (j = 0; j < detailsGrid.ColumnCount; j++)
+                {
+                    xlWorkSheet.Cells[i + 3, j+1] = detailsGrid[j, i].Value;
+                }
+            }
+
+
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.FileName = "ProjectDetails"+currentProjectName+".xls";
+            sfd.Filter = "Excel files (*.xls)|*.xls|All Files(*.*)|*.*";
+            sfd.FilterIndex = 2;
+            sfd.InitialDirectory = @"C:\Users\Public\Documents\";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                xlWorkBook.SaveAs(sfd.FileName, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+            }
+
+            xlWorkBook.Close(false, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+
+        }
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
        
     }
 }
